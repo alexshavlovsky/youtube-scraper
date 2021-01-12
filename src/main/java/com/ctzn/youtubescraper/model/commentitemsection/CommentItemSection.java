a065590e63ed2aa3f7e2a883ce55f869e04ae497
alexshavlovsky <alexshavlovsky1@gmail.com>
2021-01-12 09:18:05 +0300

    implement getting of the reply continuations map

--- src/main/java/com/ctzn/youtubescraper/model/commentitemsection/CommentItemSection.java
+++ src/main/java/com/ctzn/youtubescraper/model/commentitemsection/CommentItemSection.java
@@ -4,7 +4,10 @@ import com.ctzn.youtubescraper.model.CommentThreadHeader;
 import com.ctzn.youtubescraper.model.ContinuationData;
 import lombok.Value;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Value
@@ -16,51 +19,86 @@ public class CommentItemSection {
     public String sectionIdentifier;
 
     private static ContinuationData newContinuationData(NextContinuationData nextContinuationData) {
-        return new ContinuationData(nextContinuationData.getContinuation(), nextContinuationData.getClickTrackingParams());
+        return new ContinuationData(nextContinuationData.continuation, nextContinuationData.clickTrackingParams);
     }
 
     private String joinRuns(List<Run> runs) {
-        return runs.stream().map(Run::getText).collect(Collectors.joining());
-    }
-
-    public int commentsCount() {
-        return contents.size();
-    }
-
-    public boolean hasContinuation() {
-        return continuations != null && !continuations.isEmpty();
-    }
-
-    public ContinuationData nextContinuation() {
-        if (!hasContinuation()) throw new IllegalStateException();
-        return newContinuationData(continuations.get(0).getNextContinuationData());
+        return runs.stream().map(r -> r.text).collect(Collectors.joining());
     }
 
     public boolean hasHeader() {
         return header != null;
     }
 
+    public boolean hasComments() {
+        return contents != null && !contents.isEmpty();
+    }
+
+    public boolean hasContinuation() {
+        return continuations != null && !continuations.isEmpty();
+    }
+
+    public boolean hasReplies() {
+        return sumReplyCounters() > 0;
+    }
+
+    public int countComments() {
+        return hasComments() ? contents.size() : 0;
+    }
+
+    public ContinuationData nextContinuation() {
+        if (!hasContinuation()) throw new IllegalStateException();
+        return newContinuationData(continuations.get(0).nextContinuationData);
+    }
+
     public CommentThreadHeader getHeader() {
         if (!hasHeader()) throw new IllegalStateException();
-        CommentsHeaderRenderer headerContext = header.getCommentsHeaderRenderer();
-        String commentsCountText = joinRuns(headerContext.getCountText().getRuns());
+        CommentsHeaderRenderer headerContext = header.commentsHeaderRenderer;
+        String commentsCountText = joinRuns(headerContext.countText.runs);
         List<ContinuationData> continuations = headerContext.sortMenu.sortFilterSubMenuRenderer.subMenuItems.stream()
-                .map(i -> i.getContinuation().getReloadContinuationData()).map(CommentItemSection::newContinuationData).collect(Collectors.toList());
+                .map(i -> i.continuation.reloadContinuationData).map(CommentItemSection::newContinuationData).collect(Collectors.toList());
         return new CommentThreadHeader(continuations.get(0), continuations.get(1), commentsCountText);
     }
 
-    public void printComments() {
+    public int sumReplyCounters() {
+        if (!hasComments()) return 0;
+        return contents.stream().mapToInt(c -> c.commentThreadRenderer.comment.commentRenderer.replyCount).sum();
+    }
+
+    public int countReplyContinuations() {
+        if (!hasComments()) return 0;
+        return contents.stream().map(c -> c.commentThreadRenderer)
+                .mapToInt(c -> c.replies == null ? 0 : c.replies.commentRepliesRenderer.continuations.size()).sum();
+    }
+
+    public Map<String, ContinuationData> getReplyContinuationsMap() {
+        if (!hasComments()) return Collections.emptyMap();
+        Map<String, ContinuationData> map = new LinkedHashMap<>();
         for (Content content : contents) {
-            CommentThreadRenderer commentContext = content.getCommentThreadRenderer();
-            CommentRenderer comment = commentContext.getComment().getCommentRenderer();
+            CommentThreadRenderer commentContext = content.commentThreadRenderer;
+            if (commentContext.replies != null) {
+                CommentRenderer comment = commentContext.comment.commentRenderer;
+                CommentRepliesRenderer replies = commentContext.replies.commentRepliesRenderer;
+                map.put(comment.commentId,
+                        newContinuationData(replies.continuations.get(0).nextContinuationData));
+            }
+        }
+        return map;
+    }
+
+    public void printComments() {
+        if (!hasComments()) return;
+        for (Content content : contents) {
+            CommentThreadRenderer commentContext = content.commentThreadRenderer;
+            CommentRenderer comment = commentContext.comment.commentRenderer;
             System.out.println("================================================================================");
-            System.out.println("CommentId:  " + comment.getCommentId());
+            System.out.println("CommentId:  " + comment.commentId);
             System.out.println(String.format("Author:     %s %s (%s)",
-                    comment.getAuthorText().getSimpleText(),
-                    comment.getAuthorEndpoint().getBrowseEndpoint().getCanonicalBaseUrl(),
-                    joinRuns(comment.getPublishedTimeText().runs)));
-            System.out.println("Text:       " + joinRuns(comment.getContentText().getRuns()));
-            System.out.println("Like/Reply: " + comment.getLikeCount() + " / " + comment.getReplyCount());
+                    comment.authorText.simpleText,
+                    comment.authorEndpoint.browseEndpoint.canonicalBaseUrl,
+                    joinRuns(comment.publishedTimeText.runs)));
+            System.out.println("Text:       " + joinRuns(comment.contentText.runs));
+            System.out.println("Like/Reply: " + comment.likeCount + " / " + comment.replyCount);
         }
     }
 }

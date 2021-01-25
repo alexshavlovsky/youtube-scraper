62048592ba71c8aaf340245787b685c325f7a908
alexshavlovsky <alexshavlovsky1@gmail.com>
2021-01-25 17:00:56 +0300

    add all channel's comments example

---
+++ src/main/java/com/ctzn/youtubescraper/ChannelAllCommentsExample.java
@@ -0,0 +1,29 @@
+package com.ctzn.youtubescraper;
+
+import com.ctzn.youtubescraper.exception.ScraperException;
+import com.ctzn.youtubescraper.model.ChannelDTO;
+import com.ctzn.youtubescraper.model.VideoDTO;
+import com.ctzn.youtubescraper.runner.ChannelVideosCollector;
+import com.ctzn.youtubescraper.runner.RunnerFactory;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+public class ChannelAllCommentsExample {
+
+    static {
+        System.setProperty("java.util.logging.config.file", "logging-info.properties");
+    }
+
+    public static void main(String[] args) throws ScraperException {
+        String chanelId = "UCksTNgiRyQGwi2ODBie8HdA";
+        ChannelVideosCollector collector = new ChannelVideosCollector(chanelId);
+        ChannelDTO channel = collector.call();
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        channel.videos.stream().map(VideoDTO::getVideoId)
+                .map(RunnerFactory::newNewestCommentsFirstFileAppenderRunner).forEach(executor::submit);
+        executor.shutdown();
+    }
+}

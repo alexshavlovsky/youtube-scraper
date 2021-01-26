00f8a5eb4106a09f90c1bed95179455300d40e29
alexshavlovsky <alexshavlovsky1@gmail.com>
2021-01-26 19:05:40 +0300

    import hibernate and h2db and setup entity relationships

---
+++ src/main/java/com/ctzn/youtubescraper/entity/CommentEntity.java
@@ -0,0 +1,34 @@
+package com.ctzn.youtubescraper.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "comments")
+public class CommentEntity {
+    @Id
+    @EqualsAndHashCode.Include
+    public String commentId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "video_id")
+    public VideoEntity video;
+    public String authorText;
+    public String authorUrl;
+    public String publishedTimeText;
+    @Lob
+    public String text;
+    public int likeCount;
+    public int replyCount;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    public CommentEntity parent;
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    List<CommentEntity> replies;
+}

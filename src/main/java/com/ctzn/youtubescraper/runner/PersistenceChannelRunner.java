6aa269c8604d4d3ac1e028b1fa4ad009b3d7ce1d
alexshavlovsky <ctzn@tut.by>
2021-01-27 16:35:37 +0300

    implement persistence channel runner

---
+++ src/main/java/com/ctzn/youtubescraper/runner/PersistenceChannelRunner.java
@@ -0,0 +1,50 @@
+package com.ctzn.youtubescraper.runner;
+
+import com.ctzn.youtubescraper.db.PersistenceContext;
+import com.ctzn.youtubescraper.entity.ChannelEntity;
+import com.ctzn.youtubescraper.entity.VideoEntity;
+import com.ctzn.youtubescraper.exception.ScraperException;
+import com.ctzn.youtubescraper.model.ChannelDTO;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class PersistenceChannelRunner implements Callable<Void> {
+
+    private final String channelId;
+
+    public PersistenceChannelRunner(String channelId) {
+        this.channelId = channelId;
+    }
+
+    private static Map<String, VideoEntity> grabChannelData(String chanelId) throws ScraperException {
+        ChannelVideosCollector collector = new ChannelVideosCollector(chanelId);
+        ChannelDTO channel = collector.call();
+        ChannelEntity channelEntity = ChannelEntity.fromChannelDTO(channel);
+        List<VideoEntity> videoEntities = channel.getVideos().stream().map(v -> VideoEntity.fromVideoDTO(v, channelEntity)).collect(Collectors.toList());
+        PersistenceContext.commitTransaction(session -> {
+            session.saveOrUpdate(channelEntity);
+            videoEntities.forEach(session::saveOrUpdate);
+        });
+        return videoEntities.stream().collect(Collectors.toMap(VideoEntity::getVideoId, e -> e));
+    }
+
+    private static void grabComments(Map<String, VideoEntity> videoEntityMap) throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        videoEntityMap.keySet().stream().map(videoId -> new PersistenceCommentRunner(videoId, videoEntityMap)).forEach(executor::submit);
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+
+    @Override
+    public Void call() throws Exception {
+        Map<String, VideoEntity> videoEntityMap = grabChannelData(channelId);
+        grabComments(videoEntityMap);
+        return null;
+    }
+}

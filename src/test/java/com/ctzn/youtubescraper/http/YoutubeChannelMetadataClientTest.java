be763f18d34ed11f27f1080074435be1532c20fb
alexshavlovsky <alexshavlovsky1@gmail.com>
2021-03-12 15:54:47 +0300

    add tests

---
+++ src/test/java/com/ctzn/youtubescraper/http/YoutubeChannelMetadataClientTest.java
@@ -0,0 +1,19 @@
+package com.ctzn.youtubescraper.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class YoutubeChannelMetadataClientTest {
+
+    @Test
+    void testIntegration() {
+        assertDoesNotThrow(() -> {
+            String channelId = "UCksTNgiRyQGwi2ODBie8HdA";
+            YoutubeChannelMetadataClient channelHttpClient = new YoutubeChannelMetadataClient(channelId);
+            assertEquals("c/bienadam", channelHttpClient.getChannelVanityName());
+        });
+    }
+
+}
